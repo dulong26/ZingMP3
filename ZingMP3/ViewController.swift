4a04ba0c9477dc7197d86900fb73a6dc79a43635
Nguyen Quynh Chi <nguyenquynhchi@Nguyens-MacBook-Pro.local>
2017-04-19 23:45:44 +0700

    Initial Commit

---
+++ ZingMP3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ZingMP3
+//
+//  Created by Nguyen Quynh Chi on 4/19/17.
+//  Copyright © 2017 silverpear. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
